95a95c60e8c85add40ef8b0633d9e466c6563bc1
gxx <562606139@qq.com>
2019-02-12 19:02:17 +0800

    init

--- day1/index.py
+++ day1/index.py
@@ -1,3 +1,9 @@
 import numpy as np
-import pandas as pa
+import pandas as pd
 
+dataset = pd.read_csv('data.csv')
+X = dataset.iloc[ : , :-1].values
+Y = dataset.iloc[ : , 3].values
+
+print(X)
+print(Y)
