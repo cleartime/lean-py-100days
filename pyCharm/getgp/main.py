7941e9c337c2312f5ab1df97c20e0f39db0bd4b9
gxx <562606139@qq.com>
2019-02-27 11:26:16 +0800

    0226

---
+++ pyCharm/getgp/main.py
@@ -0,0 +1,26 @@
+# 基本信息
+import numpy as np
+import pandas as pd
+from pandas import Series, DataFrame
+
+# 股票数据的读取
+import pandas_datareader as pdr
+
+# 可视化
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from datetime import  datetime
+
+start  = datetime(2015,9,20)
+alibaba = pdr.get_data_yahoo('BABA', start = start)
+amazon = pdr.get_data_yahoo('AMZN', start = start)
+
+# alibaba.to_csv('./baba.csv')
+# amazon.to_csv('./amazon.csv')
+
+# alibaba['Adj Close'].plot(legend=True)
+# amazon['Adj Close'].plot(legend=True)
+alibaba['high-low'] = alibaba['High'] - alibaba['Low']
+alibaba['high-low'].plot(legend=True)
+plt.show()
